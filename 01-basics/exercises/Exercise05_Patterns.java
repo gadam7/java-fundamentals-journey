9cc7cf499032772aebdd48f70b288d954b6fce4a
Georgios Adamidis <g.adamidis1985@gmail.com>
2026-01-05 13:03:57 +0200

    Add exercises for Phase 1

---
+++ 01-basics/exercises/Exercise05_Patterns.java
@@ -0,0 +1,47 @@
+/**
+ * EXERCISE 5: Number Patterns
+ * 
+ * Create methods to print different number patterns using loops.
+ * 
+ * Requirements:
+ * Print the following patterns:
+ * 
+ * Pattern 1 (Square):
+ * 1 2 3 4 5
+ * 1 2 3 4 5
+ * 1 2 3 4 5
+ * 1 2 3 4 5
+ * 1 2 3 4 5
+ * 
+ * Pattern 2 (Right Triangle):
+ * 1
+ * 1 2
+ * 1 2 3
+ * 1 2 3 4
+ * 1 2 3 4 5
+ * 
+ * Pattern 3 (Inverted Triangle):
+ * 1 2 3 4 5
+ * 1 2 3 4
+ * 1 2 3
+ * 1 2
+ * 1
+ * 
+ * BONUS: Create a pyramid pattern with stars
+ * 
+ * DIFFICULTY: Medium
+ */
+public class Exercise05_Patterns {
+    
+    // TODO: Implement printSquare method
+    
+    // TODO: Implement printRightTriangle method
+    
+    // TODO: Implement printInvertedTriangle method
+    
+    // TODO: BONUS - Implement printPyramid method
+    
+    public static void main(String[] args) {
+        // TODO: Call all your pattern methods
+    }
+}
